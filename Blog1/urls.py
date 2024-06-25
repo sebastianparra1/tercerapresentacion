c0ebc754d1cd7045fb72720480126cab11dffe42
sebastianparra1 <166070659+sebastianparra1@users.noreply.github.com>
2024-06-24 23:09:56 -0400

    Add files via upload
    Blog1

---
+++ Blog1/urls.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for Blog1 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+from django.views.generic import TemplateView
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('Posts.urls')),
+    path("accounts/", include("django.contrib.auth.urls")), #new
+    path('iniciarsesion.html', TemplateView.as_view(template_name='iniciarsesion.html'), name='iniciar_sesion'),
+    path('accounts/', include('django.contrib.auth.urls')) #new
+    
+
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
